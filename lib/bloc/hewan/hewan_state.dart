dc8fa0706456622571e40e5069ae5e76ed937070
PannnTastic <pannntastic@gmail.com>
2026-05-01 19:28:19 +0700

    feat: implement HewanBloc to manage CRUD operations for hewan data

--- lib/bloc/hewan/hewan_state.dart
+++ lib/bloc/hewan/hewan_state.dart
@@ -2,9 +2,31 @@ part of 'hewan_bloc.dart';
 
 sealed class HewanState extends Equatable {
   const HewanState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class HewanInitial extends HewanState {}
+
+final class HewanLoading extends HewanState {}
+
+final class HewanLoaded extends HewanState {
+  final List<HewanModel> hewanList;
+
+  const HewanLoaded(this.hewanList);
+
+  @override
+  List<Object> get props => [hewanList];
+}
+
+final class HewanCreatedSuccess extends HewanState {}
+
+final class HewanError extends HewanState {
+  final String message;
+
+  const HewanError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
