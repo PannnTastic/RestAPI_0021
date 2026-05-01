32b83b983c4e87831984bd1a18658b75fd2a3ba2
PannnTastic <pannntastic@gmail.com>
2026-05-01 19:28:28 +0700

    feat: implement HewanBloc to manage CRUD operations for hewan data

--- lib/bloc/hewan/hewan_bloc.dart
+++ lib/bloc/hewan/hewan_bloc.dart
@@ -1,13 +1,64 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import '../../data/repositories/hewan_repository.dart';
+import '../../data/models/hewan_model.dart';
+import 'dart:developer' as developer;
 
 part 'hewan_event.dart';
 part 'hewan_state.dart';
 
 class HewanBloc extends Bloc<HewanEvent, HewanState> {
-  HewanBloc() : super(HewanInitial()) {
-    on<HewanEvent>((event, emit) {
-      // TODO: implement event handler
+  final HewanRepository repository;
+
+  HewanBloc({required this.repository}) : super(HewanInitial()) {
+    on<FetchHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        final list = await repository.getAllHewan();
+        emit(HewanLoaded(list));
+        developer.log('✓ FetchHewan: ${list.length} data dimuat', name: 'HewanBloc');
+      } catch (e) {
+        emit(HewanError(e.toString()));
+        developer.log('✗ FetchHewan Error: $e', name: 'HewanBloc');
+      }
+    });
+
+    on<CreateHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        await repository.createHewan(event.data);
+        emit(HewanCreatedSuccess());
+        developer.log('✓ CreateHewan: Berhasil menambah data', name: 'HewanBloc');
+        add(const FetchHewan());
+      } catch (e) {
+        emit(HewanError(e.toString()));
+        developer.log('✗ CreateHewan Error: $e', name: 'HewanBloc');
+      }
+    });
+
+    on<UpdateHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        await repository.updateHewan(event.id, event.data);
+        emit(HewanCreatedSuccess());
+        developer.log('✓ UpdateHewan: id=${event.id} berhasil diupdate', name: 'HewanBloc');
+        add(const FetchHewan());
+      } catch (e) {
+        emit(HewanError(e.toString()));
+        developer.log('✗ UpdateHewan Error: $e', name: 'HewanBloc');
+      }
+    });
+
+    on<DeleteHewan>((event, emit) async {
+      try {
+        await repository.deleteHewan(event.id);
+        emit(HewanCreatedSuccess());
+        developer.log('✓ DeleteHewan: id=${event.id} berhasil dihapus', name: 'HewanBloc');
+        add(const FetchHewan());
+      } catch (e) {
+        emit(HewanError(e.toString()));
+        developer.log('✗ DeleteHewan Error: $e', name: 'HewanBloc');
+      }
     });
   }
 }
