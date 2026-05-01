93f5310121329f40b556bf3ca4b734ca4de1d836
PannnTastic <pannntastic@gmail.com>
2026-05-01 22:46:32 +0700

    feat: implement CRUD operations for Hewan with BLoC state management and UI pages

--- lib/ui/widgets/custom_widget.dart
+++ lib/ui/widgets/custom_widget.dart
@@ -6,6 +6,10 @@ class CustomTextField extends StatefulWidget {
   final bool isPassword;
   final String? Function(String?)? validator;
   final IconData prefixIcon;
+  final String? hintText;
+  final TextInputType keyboardType;
+  final bool readOnly;
+  final VoidCallback? onTap;
 
   const CustomTextField({
     super.key,
@@ -14,6 +18,10 @@ class CustomTextField extends StatefulWidget {
     this.isPassword = false,
     this.validator,
     this.prefixIcon = Icons.text_fields,
+    this.hintText,
+    this.keyboardType = TextInputType.text,
+    this.readOnly = false,
+    this.onTap,
   });
 
   @override
@@ -44,82 +52,98 @@ class _CustomTextFieldState extends State<CustomTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: _isFocused
-                ? Colors.green.withOpacity(0.3)
-                : Colors.black.withOpacity(0.05),
-            blurRadius: _isFocused ? 8 : 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-        border: Border.all(
-          color: _isFocused ? Colors.green.shade600 : Colors.grey.shade300,
-          width: 2,
-        ),
-      ),
-      child: TextFormField(
-        controller: widget.controller,
-        focusNode: _focusNode,
-        obscureText: widget.isPassword ? _obscureText : false,
-        validator: widget.validator,
-        decoration: InputDecoration(
-          labelText: widget.label,
-          labelStyle: TextStyle(
-            color: _isFocused ? Colors.green.shade600 : Colors.grey.shade600,
-            fontSize: 14,
-          ),
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-          border: InputBorder.none,
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(14),
-            borderSide: BorderSide.none,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(14),
-            borderSide: BorderSide.none,
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(14),
-            borderSide: BorderSide.none,
-          ),
-          focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(14),
-            borderSide: BorderSide.none,
-          ),
-          prefixIcon: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Icon(
-              widget.prefixIcon,
-              color: _isFocused ? Colors.green.shade600 : Colors.grey.shade400,
-              size: 22,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Label di atas field
+        Padding(
+          padding: const EdgeInsets.only(left: 4, bottom: 6),
+          child: Text(
+            widget.label,
+            style: TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w600,
+              color: _isFocused ? Colors.green.shade700 : Colors.grey.shade700,
             ),
           ),
-          suffixIcon: widget.isPassword
-              ? IconButton(
-                  icon: Icon(
-                    _obscureText
-                        ? Icons.visibility_off_outlined
-                        : Icons.visibility_outlined,
-                    color: Colors.grey.shade500,
-                    size: 20,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
-                  },
-                )
-              : null,
-          hintStyle: TextStyle(color: Colors.grey.shade300, fontSize: 14),
         ),
-      ),
+        // Field
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14),
+            boxShadow: [
+              BoxShadow(
+                color: _isFocused
+                    ? Colors.green.withOpacity(0.25)
+                    : Colors.black.withOpacity(0.05),
+                blurRadius: _isFocused ? 8 : 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+            border: Border.all(
+              color:
+                  _isFocused ? Colors.green.shade600 : Colors.grey.shade300,
+              width: 1.5,
+            ),
+          ),
+          child: TextFormField(
+            controller: widget.controller,
+            focusNode: _focusNode,
+            obscureText: widget.isPassword ? _obscureText : false,
+            validator: widget.validator,
+            keyboardType: widget.keyboardType,
+            readOnly: widget.readOnly,
+            onTap: widget.onTap,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey.shade800,
+              fontWeight: FontWeight.w500,
+            ),
+            decoration: InputDecoration(
+              hintText: widget.hintText ?? 'Masukkan ${widget.label.toLowerCase()}',
+              hintStyle: TextStyle(
+                color: Colors.grey.shade400,
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 14,
+              ),
+              border: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              focusedErrorBorder: InputBorder.none,
+              prefixIcon: Icon(
+                widget.prefixIcon,
+                color: _isFocused
+                    ? Colors.green.shade600
+                    : Colors.grey.shade400,
+                size: 20,
+              ),
+              suffixIcon: widget.isPassword
+                  ? IconButton(
+                      icon: Icon(
+                        _obscureText
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        color: Colors.grey.shade500,
+                        size: 20,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    )
+                  : null,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
