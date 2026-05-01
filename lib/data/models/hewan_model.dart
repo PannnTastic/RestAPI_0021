9db8b06f253b44236fd2f12647b0170680aecf69
PannnTastic <pannntastic@gmail.com>
2026-05-01 22:46:49 +0700

    feat: implement CRUD operations and UI for hewan management using BLoC pattern

--- lib/data/models/hewan_model.dart
+++ lib/data/models/hewan_model.dart
@@ -1,34 +1,33 @@
 import 'package:equatable/equatable.dart';
 
-class HewanModel extends Equatable{
+class HewanModel extends Equatable {
   final int id;
   final String nama;
   final String jenis;
-  final String tanggalLahir;
+  final String? tanggalLahir;
   final int harga;
   final String status;
 
-  HewanModel({
+  const HewanModel({
     required this.id,
     required this.nama,
     required this.jenis,
-    required this.tanggalLahir,
+    this.tanggalLahir,
     required this.harga,
     required this.status,
   });
 
-  factory HewanModel.fromJson(Map<String,dynamic> json){
+  factory HewanModel.fromJson(Map<String, dynamic> json) {
     return HewanModel(
-      id: json["id"],
-      nama: json["nama"],
-      jenis: json["jenis"],
-      tanggalLahir: json["tanggalLahir"],
-      harga: json["harga"],
-      status: json["status"]
+      id: json["id"] as int,
+      nama: json["nama"] as String? ?? '',
+      jenis: json["jenis"] as String? ?? '',
+      tanggalLahir: json["tanggal_lahir"] as String?,
+      harga: json["harga"] as int? ?? 0,
+      status: json["status"] as String? ?? '',
     );
   }
 
   @override
   List<Object?> get props => [id, nama, jenis, tanggalLahir, harga, status];
-  
 }
