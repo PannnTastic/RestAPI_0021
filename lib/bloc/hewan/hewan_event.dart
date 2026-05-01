a040d2e5a2c545181e3994fe8e512f5e10dc40c1
PannnTastic <pannntastic@gmail.com>
2026-05-01 19:28:24 +0700

    feat: implement HewanBloc and events for CRUD operations on hewan data

--- lib/bloc/hewan/hewan_event.dart
+++ lib/bloc/hewan/hewan_event.dart
@@ -6,3 +6,35 @@ sealed class HewanEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class FetchHewan extends HewanEvent {
+  const FetchHewan();
+}
+
+class CreateHewan extends HewanEvent {
+  final Map<String, dynamic> data;
+
+  const CreateHewan({required this.data});
+
+  @override
+  List<Object> get props => [data];
+}
+
+class UpdateHewan extends HewanEvent {
+  final int id;
+  final Map<String, dynamic> data;
+
+  const UpdateHewan({required this.id, required this.data});
+
+  @override
+  List<Object> get props => [id, data];
+}
+
+class DeleteHewan extends HewanEvent {
+  final int id;
+
+  const DeleteHewan({required this.id});
+
+  @override
+  List<Object> get props => [id];
+}
